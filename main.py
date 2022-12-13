84d2fa9ea6c200c46c36ac680e7663550124ad96
Ishaaq Ollite <iollite@hotmail.com>
2022-12-13 18:14:10 +0000

    +/- is_prime() is now more readable + generate_prime_number()

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
+import random
+
 def is_prime(number:int):
     """Checks if a number is prime"""
-    if number > 1: # Make sure number is bigger than 1
-        
-        # Check if number is divisible by any number between 2 and number
-        for i in range(2, number): 
-            if (number % i) == 0: 
-                return False
-        else:
-            return True
-    else:
+
+    # Make sure number is positive
+    if not number > 1:
         return False
     
-for x in range(1000):
-    if is_prime(x):
-        print(x)
+    # Check if number is divisible by any number between 2 and itself
+    for i in range(2, number): 
+        if (number % i) == 0: 
+            return False
+    
+    return True
+
+    # A test for the above function (ignore this)
+    # for x in range(10000):
+    #   if is_prime(x):
+    #       print(f"{x} is prime")
+
+def generate_prime_number():
+    """ Generates a prime number by generating a random number and checking if it is prime """
+    
+    # This may be innefficient, but it works :D
+    # (It is fast enough for our purposes)
+    while True:
+        num = random.randint(2, 1000)
+        if is_prime(num):
+            return num
+        
+    
+        
+
+
