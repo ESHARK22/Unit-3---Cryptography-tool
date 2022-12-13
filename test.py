8fa3cebb39a3b5f7dfe54ecbe4d82bb590a46654
Ishaaq Ollite <iollite@hotmail.com>
2022-12-13 21:45:06 +0000

    Now testing acctual encryption

--- test.py
+++ test.py
@@ -1,54 +1,32 @@
-# send data to localhost:5789
 
-import socket
-import time
-import random
-HOST = 'localhost'
-PORT = 5789
+key = 132456748945676453423
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create a socket object
-s.connect((HOST, PORT)) # Connect to the server
 
-while True:
-    # A bunch of random data of strings and numbers
-    random_data = ""
-    for x in range(random.randint(1, 10000)):
-        random_data += chr(random.randint(0, 10000))
+def encrypt(key, message):
+    # Reorganise alphabet to be in key order and replace spaces
+    
+    alphabet = {}
+    
+    
 
-    random_data = bytes(random_data, "UTF-8")
-    # make it 1024 bytes long
-    if len(random_data) > 1024:
-        for x in range(1024 - len(random_data)):
-            # send random data to the server in small chunks
-            s.sendall(random_data[x:x+1])
+
         
-    else:
-        while len(random_data) < 1024:
-            random_data += b" "
-        s.sendall(random_data)
         
-    time.sleep(0.1) # wait 0.1 seconds
-    
-    
-    
-#     # Listen on localhost at port 5789 for incoming connections which will send messages separated by "[EOL]".
+        
+    # Encrypt message
+    encrypted_message = ""
+    for letter in message:
+        encrypted_message += alphabet[letter]
+    return encrypted_message
 
-# import socket
 
-# HOST = 'localhost'
-# PORT = 5789
 
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create a socket object
-# s.bind((HOST, PORT)) # Bind to the port
-# s.listen(1) # Tells the socket to listen for incoming connections
-# conn, addr = s.accept() # Accepts the connection
-# print('Connected by', addr) # Prints the address of the client
+def decrypt(key, message):
+    pass
+message = "hello world"
+encrypted = encrypt(key, message)
+# decrypted = decrypt(key, encrypted)
 
-# while True:
-#     data = conn.recv(1024) # Receives the data
-#     if not data:
-#         break
-#     print(data.decode("utf-8")) # Prints the data
-    
-# conn.close() # Closes the connection
-
+print(f"Message: {message}")
+print(f"Encrypted: {encrypted}")
+# print(f"Decrypted: {decrypted}")
