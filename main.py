f18c7e6f00dba215f1b787f3bb61885a6bbc5d70
Ishaaq Ollite <iollite@hotmail.com>
2022-12-13 19:29:14 +0000

    added laice and bob stuff from book

--- main.py
+++ main.py
@@ -19,8 +19,6 @@ def is_prime(number:int):
     
     return True
 
-
-
 def generate_prime_number():
     """ Generates a prime number by generating a random number and checking if it is prime """
     
@@ -31,7 +29,31 @@ def generate_prime_number():
         if is_prime(num):
             return num
         
-    
+def greatest_common_divisor(a:int, b:int):
+    """ Finds the greatest common divisor of two numbers """
+    while b: # while b is not 0
         
+        a, b = b, a % b 
+    return a
+    
+# to create a key we must first generate two prime numbers
+p = generate_prime_number()
+q = generate_prime_number()
+
+# then we multiply them to get n
+n = p * q
+# n is one part of the public key
 
 
+# https://www.thefreedictionary.com/totient
+_totient = "(Mathematics) a quantity of numbers less than, and sharing no common factors with, a given number"
+# '-> a prime number that is coprime to n
+
+_coprime = "when two numbers have no common factors other than 1."
+# '-> https://www.mathsisfun.com/definitions/coprime.html
+# two numbers are coprime if their greatest common divisor is 1
+
+# then we need to find the "totient"
+totient = (p - 1) * (q - 1)
+
+print(f"p: {p}, q: {q}, n: {n}, totient: {totient}")
--- test.py
+++ test.py
@@ -1,28 +1,53 @@
-import random
+ALICE_num = 3
+BOB_num = 6
 
-alice_PRIV = random.randint(50, 100)
-bob_PRIV = random.randint(50, 100)
+# One way function vars
+MOD = 11
+Y = 7
 
-
-def modular(mod_size, x):
+def mod(mod:int, num:int):
+    """ 
+    Returns the mod of a number
+    (the remainder of the number divided by the mod)
+    Modular arithmatics goes in circles, like a clock
+    5 o'clock + 2 = 7 o'clock 
+    5 + 2 = 7 (mod 12)
     """
-    This is like a clock; it goes from 1 to 12 and then starts over again. 
-    You would never say 17 o'clock, you would say 5 o'clock.
-    The numbers go round and round in a circle.
-    9+8 = 5 (mod 12) aka 9 o'clock + 8 o'clock = 5 o'clock
-    ->modeular maths :D
-    """
+    return num % mod # returns the remainder of the number divided by the mod
 
-    ans = x % mod_size
+
+def one_way_function(Y:int, _mod:int, num:int):
+    """
+    A one way function is a function that is easy to compute, but hard to reverse.
+    This function is one way because it is easy to calculate a number powered (mod x) 
+        but hard to calculate the number that powered (mod x) to get the result.
+        Currently the only known way to reverse this function is to brute force it.
+    """
+    # y^x (mod _mod)
+    # Note: Y must be smaller than the mod
     
-    # modular function is used to find the remainder of a division
-    # If its 0 add 1 to the answer
-    if ans == 0:
-        ans += 1
-        print("mod_sizeular: ", ans)
-        return ans
-    else:
-        print("mod_sizeular: ", ans)
-        return ans
-    
-print(modular(7, 243))
+    powered = Y ** num # Y to the power of num
+    return mod(_mod, powered) # powered (mod _mod)
+
+# Alice and Bob both have the same mod
+# Alice and Bob both have the same number
+# Alice and Bob both have the same one way function
+# They can both calculate the same result
+# But they can't reverse the result to get the number
+
+# Alice generates her public key
+ALICE_public_key = one_way_function(Y, MOD, BOB_num)
+BOB_public_key = one_way_function(Y, MOD, ALICE_num)
+
+print(ALICE_public_key)
+print(BOB_public_key)
+
+# Alice sends her public key to Bob
+# Bob sends his public key to Alice
+
+# They can both calculate the same result
+ALICE_result = one_way_function(ALICE_public_key, MOD, ALICE_num)
+BOB_result = one_way_function(BOB_public_key, MOD, BOB_num)
+
+print(ALICE_result)
+print(BOB_result)
