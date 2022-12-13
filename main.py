9df3198491e79c57dc06e1b22368beae622391e3
Ishaaq Ollite <iollite@hotmail.com>
2022-12-13 18:07:27 +0000

    Added prime number generator

--- main.py
+++ main.py
@@ -1,51 +1,16 @@
-import random
-
-General_mod = 7
-
-
-alice_PRIV = 3
-bob_PRIV = 6
-
-
-def one_way_func(Y, x, mod_size):
-    """
-    A one way function is very easy to calc, but hard to reverse.
-    e.g: mixing paint (easy to mix, hard to unmix).
+def is_prime(number:int):
+    """Checks if a number is prime"""
+    if number > 1: # Make sure number is bigger than 1
+        
+        # Check if number is divisible by any number between 2 and number
+        for i in range(2, number): 
+            if (number % i) == 0: 
+                return False
+        else:
+            return True
+    else:
+        return False
     
-    Modular arithmatics has a lot of one way functions
-    An example of Modular arithmatics is 12 hour time
-    8 o'clock + 7 o'clock != 15 o'clock (you wouldnt say 15 o'clock)
-                         ->= 3 o'clock  (You would say 3 o'clock)
-    ---->8 + 5 = 3 (mod 12)<----
-    """
-    
-    # Y^x (mod p)
-    ans = (Y**x) % mod_size
-    print("one_way_func: ", ans)
-
-   
-print(one_way_func(3, 4, General_mod)) 
-    
-def modular_calc(num:int, mod:int=General_mod):
-    """
-    To calculate modular maths, you can use division:
-        99 mod(13) = 99/13 = 7, remainder 8
-        99 = 8 (mod 13) 
-    """
-    
-    ans = num % mod
-    
-    # You dont say 0 o'clock
-    if ans == 0:
-        ans += 1
-    
-    return ans
-
-
-def power(x, y):
-    """z=x^y"""
-    z = x**y
-    return z
-
-
-print(modular_calc(7, 27))
+for x in range(1000):
+    if is_prime(x):
+        print(x)
